68db84d9ab612beee584e7337e93f2b067290ecb
John <dazajohn08@gmail.com>
2026-03-25 20:01:28 -0700

    done 2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int num, max = INT_MIN;  
-    for (int i = 0; i < 10; i++){
+    int num, max;
+    for (int i = 0; i < 10; i++) {
         cin >> num;
-        if(i == 0 || max < num){
+        if (i == 0 || num > max) {
             max = num;
         }
     }
-    cout << "max: "<< max << endl;
+    cout << "max: " << max << endl;
     return 0;
 }
